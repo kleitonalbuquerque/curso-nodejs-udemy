1a3afc6c92dd24491f9d54701a45080ca50550d6
kleitonalbuquerque <kleiton2102@gmail.com>
2019-06-08 13:54:48 -0300

    Trabalhando com view ejs

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Diretório e função de callback
-app.get('/', (req, res) => res.send('<html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Curso Node</title></head><body>Portal de Notícias</body></html>'))
+// Generate views
+app.set('view engine', 'ejs')
 
-app.get('/tecnologia', (req, res) => res.send('<html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Curso Node</title></head><body>Notícias de Tecnologia</body></html>'))
+// Directory and callback function
+app.get('/', (req, res) => 
+  res.render(''))
 
-app.get('/moda', (req, res) => res.send('<html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Curso Node</title></head><body>Notícias de moda</body></html>'))
+app.get('/tecnologia', (req, res) => 
+  res.render('secao/tecnologia'))
 
-app.get('/beleza', (req, res) => res.send('<html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Curso Node</title></head><body>Notícias de beleza</body></html>'))
+app.get('/moda', (req, res) => 
+  res.render('secao/moda'))
+
+app.get('/beleza', (req, res) => 
+  res.render('secao/beleza'))
 
 app.listen(port, () => console.log(`server listening on port ${port}!`))
